f3053ddd75b1b5a75a2fa50c9f618c9d387a5f7e
Gilad Nadav <gilad.nadav@kaltura.com>
2018-11-19 14:25:02 +0200

    FEM-2292 Fire Phoenix Errors in case of Bookmark event has failed (#26)
    * avoid accessing player from timer thread
    
    * remove player usage
    
    * comment log
    
    * fix warnings
    ignore AD_PROGRESS logs
    
    * fix import error
    
    * fix warning
    
    * fix warnings
    
    * upgrade netkit to 1.1.0
    
    * Add API Exception support in Phoenix Analytics bookmark events
    
    * remove Gson
    
    * cosmetics
    
    * remove unneeded code
    done in
    @Override
        public Enum eventType() {
            return this.type;
        }
    
    * remove import

--- mediaproviders/src/main/java/com/kaltura/playkit/plugins/ott/PhoenixAnalyticsEvent.java
+++ mediaproviders/src/main/java/com/kaltura/playkit/plugins/ott/PhoenixAnalyticsEvent.java
@@ -20,8 +20,16 @@ import com.kaltura.playkit.PKEvent;
 
 public class PhoenixAnalyticsEvent implements PKEvent {
 
+    public final PhoenixAnalyticsEvent.Type type;
+
+    public PhoenixAnalyticsEvent(PhoenixAnalyticsEvent.Type type) {
+        this.type = type;
+    }
     public enum Type {
-        REPORT_SENT
+        REPORT_SENT,
+        CONCURRENCY_ERROR,
+        BOOKMARK_ERROR,
+        ERROR
     }
 
     public static class PhoenixAnalyticsReport extends PhoenixAnalyticsEvent {
@@ -29,13 +37,39 @@ public class PhoenixAnalyticsEvent implements PKEvent {
         public final String reportedEventName;
 
         public PhoenixAnalyticsReport(String reportedEventName) {
+            super(Type.REPORT_SENT);
             this.reportedEventName = reportedEventName;
         }
     }
 
+    public static class ErrorEvent extends PhoenixAnalyticsEvent {
+
+        public final int errorCode;
+        public final String errorMessage;
+
+        public ErrorEvent(PhoenixAnalyticsEvent.Type errorType, int errorCode, String errorMessage) {
+            super(errorType);
+            this.errorCode = errorCode;
+            this.errorMessage = errorMessage;
+        }
+    }
+
+    public static class BookmarkErrorEvent extends ErrorEvent {
+
+        public BookmarkErrorEvent(int errorCode, String errorMessage) {
+            super(Type.BOOKMARK_ERROR, errorCode, errorMessage);
+        }
+    }
+
+    public static class ConcurrencyErrorEvent extends ErrorEvent {
+
+        public ConcurrencyErrorEvent(int errorCode, String errorMessage) {
+            super(Type.CONCURRENCY_ERROR, errorCode, errorMessage);
+        }
+    }
 
     @Override
     public Enum eventType() {
-        return Type.REPORT_SENT;
+        return this.type;
     }
 }
