1fb3439b355bba972d5497a7838ee5f8f8f858dc
Gilad Nadav <gilad.nadav@kaltura.com>
2019-09-22 12:23:57 +0300

    add
    public enum KalturaRecordingType {
        SINGLE,
        SEASON,
        SERIES
    }
    
    add public class KalturaRecordingAsset extends KalturaMediaAsset

---
+++ mediaproviders/src/main/java/com/kaltura/playkit/providers/api/phoenix/model/KalturaRecordingAsset.java
@@ -0,0 +1,56 @@
+/*
+ * ============================================================================
+ * Copyright (C) 2017 Kaltura Inc.
+ * 
+ * Licensed under the AGPLv3 license, unless a different license for a
+ * particular library is specified in the applicable library path.
+ * 
+ * You may obtain a copy of the License at
+ * https://www.gnu.org/licenses/agpl-3.0.html
+ * ============================================================================
+ */
+
+package com.kaltura.playkit.providers.api.phoenix.model;
+
+public class KalturaRecordingAsset extends KalturaMediaAsset {
+    private String recordingId;
+    private KalturaRecordingType recordingType;
+    private boolean enableCatchUp;
+    private boolean enableCdvr;
+    private String epgId;
+    private long linearAssetId;
+    private boolean enableStartOver;
+    private boolean enableTrickPlay;
+
+    public String getRecordingId() {
+        return recordingId;
+    }
+
+    public KalturaRecordingType getRecordingType() {
+        return recordingType;
+    }
+
+    public boolean isEnableCatchUp() {
+        return enableCatchUp;
+    }
+
+    public boolean isEnableCdvr() {
+        return enableCdvr;
+    }
+
+    public String getEpgId() {
+        return epgId;
+    }
+
+    public long getLinearAssetId() {
+        return linearAssetId;
+    }
+
+    public boolean isEnableStartOver() {
+        return enableStartOver;
+    }
+
+    public boolean isEnableTrickPlay() {
+        return enableTrickPlay;
+    }
+}
