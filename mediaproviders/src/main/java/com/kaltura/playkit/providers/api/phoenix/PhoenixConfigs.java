8f432fe99118f967ec6cfd3f12123e5ddb4134f6
Gourav Saxena <gourav.saxena@kaltura.com>
2021-05-03 18:34:02 +0530

    - Updated BE to v6.3.0

--- mediaproviders/src/main/java/com/kaltura/playkit/providers/api/phoenix/PhoenixConfigs.java
+++ mediaproviders/src/main/java/com/kaltura/playkit/providers/api/phoenix/PhoenixConfigs.java
@@ -23,6 +23,6 @@ public class PhoenixConfigs {
     //public static String ApiPrefix = "api_v3/";
 
     public static String ClientTag = PlayKitManager.CLIENT_TAG;
-    public static String ApiVersion = "5.2.6";
+    public static String ApiVersion = "6.3.0";
 
 }
