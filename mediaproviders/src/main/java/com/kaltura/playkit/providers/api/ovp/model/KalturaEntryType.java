30f359718eba782e42032b1116560ce3e266a022
Gilad Nadav <gilad.nadav@kaltura.com>
2022-03-30 18:47:55 +0300

    fix potential crash if SDK is requested to play Youtube media entry

--- mediaproviders/src/main/java/com/kaltura/playkit/providers/api/ovp/model/KalturaEntryType.java
+++ mediaproviders/src/main/java/com/kaltura/playkit/providers/api/ovp/model/KalturaEntryType.java
@@ -24,10 +24,24 @@ public enum KalturaEntryType {
     AUTOMATIC("-1"),
     @SerializedName("1")
     MEDIA_CLIP("1"),
+    @SerializedName("2")
+    MIX("2"),
+    @SerializedName("5")
+    PLAYLIST("5"),
+    @SerializedName("6")
+    DATA("6"),
     @SerializedName("7")
     LIVE_STREAM("7"),
-    @SerializedName("5")
-    PLAYLIST("5");
+    @SerializedName("8")
+    LIVE_CHANNEL("8"),
+    @SerializedName("10")
+    DOCUMENT("10"),
+    @SerializedName("conference.CONFERENCE_ENTRY_SERVER")
+    CONFERENCE_ENTRY_SERVER("conference.CONFERENCE_ENTRY_SERVER"),
+    @SerializedName("externalMedia.externalMedia")
+    EXTERNAL_MEDIA("externalMedia.externalMedia"),
+    @SerializedName("sip.SIP_ENTRY_SERVER")
+    SIP_ENTRY_SERVER("sip.SIP_ENTRY_SERVER");
 
     public String type;
 
